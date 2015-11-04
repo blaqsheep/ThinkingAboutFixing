0829c28ee618761476dbc3355f64782037aaf35b
blaqsheep <odwa@projectcodex.co>
2015-11-04 12:39:15 +0200

    fixed some bugs on the app

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     issues = require('./issues');
 
+
 var app = express();
 
 var dbOptions = {
       host: 'localhost',
-      user: 'debugger',
-      password: 'passworddd',
+      user: 'root',
+      password: 'Nondlozio89',
       port: 3306,
       database: 'debugging_tips'
 };
@@ -38,10 +39,10 @@ function errorHandler(err, req, res, next) {
 //setup the handlers
 app.get('/', issues.all);
 app.get('/issues', issues.all);
-app.get('/issues/add' issues.showAdd);
+app.get('/issues/add', issues.showAdd);
 app.get('/issues/:id', issues.get);
 app.post('/issues/update/:id', issues.update);
-app.post('/issuesss', issues.add);
+app.post('/issues', issues.add);
 app.get('/issues/delete/:id', issues.delete);
 
 //this should be a post but this is only an illustration of CRUD - not on good practices
